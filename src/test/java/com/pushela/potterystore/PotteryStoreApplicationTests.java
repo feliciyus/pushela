0fb192cc595a01175ac75714958379a6df4547ae
feliciyus <jonas.kazlauskas6@gmail.com>
2023-02-24 12:53:25 +0200

    Initial commit

---
+++ src/test/java/com/pushela/potterystore/PotteryStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pushela.potterystore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotteryStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
